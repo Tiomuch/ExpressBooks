897a8a250a6919e61359787af058e48232c3fbeb
Tiomuch <artimerz@ukr.net>
2023-12-19 21:41:52 +0200

    init project

---
+++ src/routes/books.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+// Get all books
+router.get('/', (req, res) => {})
+
+// Get one book
+router.get('/:id', (req, res) => {})
+
+// Create book
+router.post('/', (req, res) => {})
+
+// Update book
+router.patch('/:id', (req, res) => {})
+
+// Delete book
+router.delete('/:id', (req, res) => {})
+
+module.exports = router
